5cdc2024441abbbdfc4a6af07e13ea0c8260cdc9
SoraSkyy <viorama@gmail.com>
2017-01-21 17:03:00 +0800

    Improved client testing

--- Server/clientTest.py
+++ Server/clientTest.py
@@ -1,6 +1,11 @@
 import socket
 
-server = 172.23.181.78
+server = '172.23.181.78'
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-self.irc.connect((self.server, 5000))
-self.connect()
+s.connect((server, 5000))
+
+while True:
+    data = s.recv(1024).decode()
+    print(data)
+    text = input()
+    s.send(text.encode())
