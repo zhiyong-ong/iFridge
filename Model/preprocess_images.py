a3383d366415bf508a2c434091d140558a0673c7
Zhi Yong <zhiyong.ong@hotmail.com>
2017-01-22 12:11:29 +0800

    varying magic numbers

--- Model/preprocess_images.py
+++ Model/preprocess_images.py
@@ -79,15 +79,15 @@ print (train_data[0].shape)
 print (train_data[1].shape)
 
 # Batch_size to train
-batch_size = 32
+batch_size = [22, 27, 32, 37, 42]
 # Number of output classes
 nb_classes = 3
 # Number of epochs to train
-nb_epoch = 5
+nb_epoch = 1
 
 
 # Number of convolutional filters to use
-nb_filters = 32
+nb_filters = 28
 # Size of pooling area for max pooling
 nb_pool = 2
 # Convolution kernel size
@@ -96,56 +96,56 @@ nb_conv = 3
 #%%
 (X, y) = (train_data[0], train_data[1])
 
+for i in range(5):
+  # STEP 1: split X and y into training and testing sets
+  X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=4)
 
-# STEP 1: split X and y into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=4)
 
+  X_train = X_train.reshape(X_train.shape[0], img_rows, img_cols, 3)
+  X_test  = X_test.reshape(X_test.shape[0], img_rows, img_cols, 3)
 
-X_train = X_train.reshape(X_train.shape[0], img_rows, img_cols, 3)
-X_test  = X_test.reshape(X_test.shape[0], img_rows, img_cols, 3)
+  X_train = X_train.astype('float32')
+  X_test  = X_test.astype('float32')
 
-X_train = X_train.astype('float32')
-X_test  = X_test.astype('float32')
+  X_train /= 255
+  X_test  /= 255
 
-X_train /= 255
-X_test  /= 255
+  print('X_train shape:', X_train.shape)
+  print(X_train.shape[0], 'train samples')
+  print(X_test.shape[0], 'test samples')
 
-print('X_train shape:', X_train.shape)
-print(X_train.shape[0], 'train samples')
-print(X_test.shape[0], 'test samples')
+  # convert class vectors to binary class matrices
+  Y_train = np_utils.to_categorical(y_train, nb_classes)
+  Y_test = np_utils.to_categorical(y_test, nb_classes)
 
-# convert class vectors to binary class matrices
-Y_train = np_utils.to_categorical(y_train, nb_classes)
-Y_test = np_utils.to_categorical(y_test, nb_classes)
+  #%%
 
-#%%
+  model = Sequential()
 
-model = Sequential()
+  model.add(Convolution2D(nb_filters, nb_conv, nb_conv,
+                          border_mode='valid',
+                          input_shape=(img_rows, img_cols, 3)))
+  convout1 = Activation('relu')
+  model.add(convout1)
+  model.add(Convolution2D(nb_filters, nb_conv, nb_conv))
+  convout2 = Activation('relu')
+  model.add(convout2)
+  model.add(MaxPooling2D(pool_size=(nb_pool, nb_pool)))
+  model.add(Dropout(0.5))
 
-model.add(Convolution2D(nb_filters, nb_conv, nb_conv,
-                        border_mode='valid',
-                        input_shape=(img_rows, img_cols, 3)))
-convout1 = Activation('relu')
-model.add(convout1)
-model.add(Convolution2D(nb_filters, nb_conv, nb_conv))
-convout2 = Activation('relu')
-model.add(convout2)
-model.add(MaxPooling2D(pool_size=(nb_pool, nb_pool)))
-model.add(Dropout(0.5))
+  model.add(Flatten())
+  model.add(Dense(128))
+  model.add(Activation('relu'))
+  model.add(Dropout(0.5))
+  model.add(Dense(nb_classes))
+  model.add(Activation('softmax'))
+  model.compile(loss='categorical_crossentropy', optimizer='adadelta', metrics=["accuracy"])
 
-model.add(Flatten())
-model.add(Dense(128))
-model.add(Activation('relu'))
-model.add(Dropout(0.5))
-model.add(Dense(nb_classes))
-model.add(Activation('softmax'))
-model.compile(loss='categorical_crossentropy', optimizer='adadelta', metrics=["accuracy"])
+  #%%
 
-#%%
+  hist = model.fit(X_train, Y_train, batch_size=batch_size[i], nb_epoch=nb_epoch,
+                verbose=1, validation_data=(X_test, Y_test))
 
-hist = model.fit(X_train, Y_train, batch_size=batch_size, nb_epoch=nb_epoch,
-              verbose=1, validation_data=(X_test, Y_test))
-      
 '''            
 hist = model.fit(X_train, Y_train, batch_size=batch_size, nb_epoch=nb_epoch,
               verbose=1, validation_split=0.2)
