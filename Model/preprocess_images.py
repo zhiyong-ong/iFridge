02e18a77924e9efa359eb73e74308486abe73f01
Yew <tangys108@gmail.com>
2017-01-22 11:51:36 +0800

    [Initial]

---
+++ Model/preprocess_images.py
@@ -0,0 +1,252 @@
+# Keras
+from keras import backend as K
+from keras.models import Sequential
+from keras.layers.core import Dense, Dropout, Activation, Flatten
+from keras.layers.convolutional import Convolution2D, MaxPooling2D
+from keras.optimizers import SGD,RMSprop,adam
+from keras.utils import np_utils
+
+import gc
+import os
+import theano
+import numpy as np
+import matplotlib as mpl
+mpl.use('TkAgg')
+import matplotlib.pyplot as plt
+
+from PIL import Image, ImageFile
+ImageFile.LOAD_TRUNCATED_IMAGES = True
+
+# SKLearn
+from sklearn.utils import shuffle
+from sklearn.cross_validation import train_test_split
+
+# Input image dimensions
+img_rows, img_cols = 100, 100
+
+# Stores list of different categories of images
+# [12, 34, 56] indicates 12 of A, 34 of B and 56 of C
+#num_images = [387, 180, 333, 185, 495, 342, 160]
+num_images = [37, 495, 28]
+num_samples = sum(num_images)
+
+# Number of channels
+img_channels = 3
+
+# Data
+# (http://learnandshare645.blogspot.sg/2016/06/feeding-your-own-data-set-into-cnn.html)
+#path1 = '/home/yewsiang/iFridge Dataset/Square Images'
+#path2 = '/home/yewsiang/iFridge Dataset/Resized Images for Training'
+path1 = '/home/yewsiang/iFridge Dataset/Square Centered Images'
+path2 = '/home/yewsiang/iFridge Dataset/Resized Centered Images for Training'
+'''
+# Resizes the raw images (only need to do once)
+imlist = os.listdir(path1)
+for file in imlist:
+    im = Image.open(path1 + '/' + file)   
+    img = im.resize((img_rows,img_cols))
+    #need to do some more processing here           
+    img.save(path2 +'/' +  file, "PNG")
+'''
+# Training images
+imlist = os.listdir(path2)
+imlist.sort()
+
+# create matrix to store flattened images
+immatrix = np.array([np.array(Image.open(path2 + '/' + im2))[:,:,0:3].flatten()
+              for im2 in imlist]) # Truncate the last bit of the image
+
+label = np.ones((num_samples,), dtype = int)
+start_idx = 0
+for i, num in enumerate(num_images):
+  for j in range(start_idx, start_idx + num):
+    label[j] = i
+  start_idx += num
+
+data, Label = shuffle(immatrix, label, random_state=2)
+train_data = [data, Label]
+
+'''
+# Verify that shuffling is done correctly
+for i in range(0,20):
+  img = data[i].reshape((img_cols, img_rows, img_channels))
+  plt.imshow(img)
+  print(Label[i])
+  plt.show()
+'''
+
+print (train_data[0].shape)
+print (train_data[1].shape)
+
+# Batch_size to train
+batch_size = 32
+# Number of output classes
+nb_classes = 3
+# Number of epochs to train
+nb_epoch = 5
+
+
+# Number of convolutional filters to use
+nb_filters = 32
+# Size of pooling area for max pooling
+nb_pool = 2
+# Convolution kernel size
+nb_conv = 3
+
+#%%
+(X, y) = (train_data[0], train_data[1])
+
+
+# STEP 1: split X and y into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=4)
+
+
+X_train = X_train.reshape(X_train.shape[0], img_rows, img_cols, 3)
+X_test  = X_test.reshape(X_test.shape[0], img_rows, img_cols, 3)
+
+X_train = X_train.astype('float32')
+X_test  = X_test.astype('float32')
+
+X_train /= 255
+X_test  /= 255
+
+print('X_train shape:', X_train.shape)
+print(X_train.shape[0], 'train samples')
+print(X_test.shape[0], 'test samples')
+
+# convert class vectors to binary class matrices
+Y_train = np_utils.to_categorical(y_train, nb_classes)
+Y_test = np_utils.to_categorical(y_test, nb_classes)
+
+#%%
+
+model = Sequential()
+
+model.add(Convolution2D(nb_filters, nb_conv, nb_conv,
+                        border_mode='valid',
+                        input_shape=(img_rows, img_cols, 3)))
+convout1 = Activation('relu')
+model.add(convout1)
+model.add(Convolution2D(nb_filters, nb_conv, nb_conv))
+convout2 = Activation('relu')
+model.add(convout2)
+model.add(MaxPooling2D(pool_size=(nb_pool, nb_pool)))
+model.add(Dropout(0.5))
+
+model.add(Flatten())
+model.add(Dense(128))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(Dense(nb_classes))
+model.add(Activation('softmax'))
+model.compile(loss='categorical_crossentropy', optimizer='adadelta', metrics=["accuracy"])
+
+#%%
+
+hist = model.fit(X_train, Y_train, batch_size=batch_size, nb_epoch=nb_epoch,
+              verbose=1, validation_data=(X_test, Y_test))
+      
+'''            
+hist = model.fit(X_train, Y_train, batch_size=batch_size, nb_epoch=nb_epoch,
+              verbose=1, validation_split=0.2)
+'''
+
+# Saving weights
+fname = "fridge-CNN-3items-centered.hdf5"
+model.save_weights(fname,overwrite=True)
+
+'''
+# Visualizing losses and accuracy
+train_loss=hist.history['loss']
+val_loss=hist.history['val_loss']
+train_acc=hist.history['acc']
+val_acc=hist.history['val_acc']
+xc=range(nb_epoch)
+
+plt.figure(1,figsize=(7,5))
+plt.plot(xc,train_loss)
+plt.plot(xc,val_loss)
+plt.xlabel('num of Epochs')
+plt.ylabel('loss')
+plt.title('train_loss vs val_loss')
+plt.grid(True)
+plt.legend(['train','val'])
+print plt.style.available # use bmh, classic,ggplot for big pictures
+plt.style.use(['classic'])
+
+plt.figure(2,figsize=(7,5))
+plt.plot(xc,train_acc)
+plt.plot(xc,val_acc)
+plt.xlabel('num of Epochs')
+plt.ylabel('accuracy')
+plt.title('train_acc vs val_acc')
+plt.grid(True)
+plt.legend(['train','val'],loc=4)
+#print plt.style.available # use bmh, classic,ggplot for big pictures
+plt.style.use(['classic'])
+
+
+score = model.evaluate(X_test, Y_test, show_accuracy=True, verbose=0)
+print('Test score:', score[0])
+print('Test accuracy:', score[1])
+print(model.predict_classes(X_test[1:5]))
+print(Y_test[1:5])
+
+def get_activations(model, layer_idx, X_batch):
+    get_activations = K.function([model.layers[0].input, K.learning_phase()], [model.layers[layer_idx].output,])
+    activations = get_activations([X_batch,0])
+    return activations
+
+# visualizing intermediate layers
+
+output_layer = get_activations(model, 1, X_train)
+output_fn = theano.function([model.layers[0].input], output_layer)
+
+# the input image
+
+input_image=X_train[0:1,:,:,:]
+print(input_image.shape)
+
+plt.imshow(input_image[0,0,:,:],cmap ='gray')
+plt.imshow(input_image[0,0,:,:])
+
+
+output_image = output_fn(input_image)
+print(output_image.shape)
+
+# Rearrange dimension so we can plot the result 
+output_image = np.rollaxis(np.rollaxis(output_image, 3, 1), 3, 1)
+print(output_image.shape)
+
+
+fig=plt.figure(figsize=(8,8))
+for i in range(32):
+    ax = fig.add_subplot(6, 6, i+1)
+    #ax.imshow(output_image[0,:,:,i],interpolation='nearest' ) #to see the first filter
+    ax.imshow(output_image[0,:,:,i],cmap=matplotlib.cm.gray)
+    plt.xticks(np.array([]))
+    plt.yticks(np.array([]))
+    plt.tight_layout()
+plt
+
+# Confusion Matrix
+
+from sklearn.metrics import classification_report,confusion_matrix
+
+Y_pred = model.predict(X_test)
+print(Y_pred)
+y_pred = np.argmax(Y_pred, axis=1)
+print(y_pred)
+
+
+
+p = model.predict_proba(X_test) # to predict probability
+
+target_names = ['class 0(No bucket)', 'class 1(Bucket)']
+print(classification_report(np.argmax(Y_test,axis=1), y_pred,target_names=target_names))
+print(confusion_matrix(np.argmax(Y_test,axis=1), y_pred))
+'''
+
+# To solve problem of 'TF_DeleteStatus'
+# (https://github.com/tensorflow/tensorflow/issues/3388)
+gc.collect()
--- Server/imagerecognize.py
+++ Server/imagerecognize.py
@@ -1,4 +1,71 @@
+# Keras
+from keras import backend as K
+from keras.models import Sequential
+from keras.layers.core import Dense, Dropout, Activation, Flatten
+from keras.layers.convolutional import Convolution2D, MaxPooling2D
+from keras.optimizers import SGD,RMSprop,adam
+from keras.utils import np_utils
+
+from PIL import Image, ImageFile
+
+import matplotlib as mpl
+mpl.use('TkAgg')
+import matplotlib.pyplot as plt
+
+import gc
+import os
+import theano
+import numpy as np
 import threading
+
+# === Global Variables ===
+# Number of channels
+img_channels = 3
+
+# Number of output classes
+nb_classes = 3
+# Number of convolutional filters to use
+nb_filters = 32
+# Size of pooling area for max pooling
+nb_pool = 2
+# Convolution kernel size
+nb_conv = 3
+
+# Filename of model
+fname = "../Model/fridge-CNN-3items-more-data.hdf5"
+
+#classes = ["Beer", "Chocolate", "Coke", "Iced lemon tea", "Nothing", "Waffle", "Yakult"]
+classes = ["Iced Lemon Tea", "Nothing", "Yakult"]
+
+# Input image dimensions
+img_rows, img_cols = 100, 100
+
+# Input information
+full_img_cols, full_img_rows = 600, 400
+new_img_cols, new_img_rows   = int(full_img_cols / 1), int(full_img_rows / 1)
+
+# Model
+model = Sequential()
+model.add(Convolution2D(nb_filters, nb_conv, nb_conv,
+                        border_mode='valid',
+                        input_shape=(img_rows, img_cols, 3)))
+convout1 = Activation('relu')
+model.add(convout1)
+model.add(Convolution2D(nb_filters, nb_conv, nb_conv))
+convout2 = Activation('relu')
+model.add(convout2)
+model.add(MaxPooling2D(pool_size=(nb_pool, nb_pool)))
+model.add(Dropout(0.5))
+
+model.add(Flatten())
+model.add(Dense(128))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(Dense(nb_classes))
+model.add(Activation('softmax'))
+
+model.load_weights(fname)
+
 
 class ImageRecognize(threading.Thread):
 	def __init__(self, cameraServer):
@@ -8,6 +75,55 @@ class ImageRecognize(threading.Thread):
 		self.cameraServer = cameraServer
 
 	def process(self, image):
+		p = ImageFile.Parser()
+
+		p.feed(image)
+		img = p.close()
+		img = img.resize((new_img_rows,new_img_cols))
+		img_array = np.array(img)[:,:,0:3]
+
+		step_size = 50
+		num_hori_steps  = int((new_img_rows - img_rows) / step_size)
+		final_hori_step = new_img_rows - step_size * num_hori_steps
+		num_vert_steps  = int((new_img_cols - img_cols) / step_size)
+		final_vert_step = new_img_cols - step_size * num_vert_steps
+
+		imgs = np.zeros((num_vert_steps * num_hori_steps, img_rows, img_cols, 3), dtype=np.uint8)
+
+		for i in range(0, num_vert_steps):
+		  for j in range(0, num_hori_steps):
+		    current_row = i * step_size
+		    current_col = j * step_size
+		    #print("[{0}, {1}] [Row: {2}][Col: {3}]".format(i, j, current_row, current_col))
+		    current_img = img_array[current_row:current_row + img_rows, current_col:current_col + img_cols,:]
+		    
+		    #if j % 2 == 0:
+		    #  plt.imshow(current_img)
+		    #  plt.show()
+		    imgs[i * num_hori_steps + j,:,:,:] = current_img
+
+		Y_pred = model.predict(imgs)
+
+		# Choose the highest probability class
+		pred_class = np.argmax(Y_pred, axis=1)
+
+		# Count the number of times each class appeared
+		count = [0] * nb_classes
+		for i in range(0, len(Y_pred)):
+		  count[pred_class[i]] += 1
+		  #print(classes[pred_class[i]])
+		  #plt.imshow(imgs[i,:,:,:])
+		  #plt.show()
+
+		# Statistics of image patches
+		for i in range(0, nb_classes):
+		  print(classes[i] + ": " + str(count[i]))
+
+		'''
+		f.write(image)
+		f.close()
+		Image.open("capture.jpg")
+		'''
 		print('Image processed!')
 
 	def run(self):
